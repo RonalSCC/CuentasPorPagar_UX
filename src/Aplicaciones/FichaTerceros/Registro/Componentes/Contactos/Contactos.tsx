90f78caeae23e3853d668ab5cf9252dfaf795406
CPZ_CristianPerez <cristian.perez@sinco.com.co>
2022-12-27 16:51:30 -0500

    Pantalla Formulario agregar contacto

--- src/Aplicaciones/FichaTerceros/Registro/Componentes/Contactos/Contactos.tsx
+++ src/Aplicaciones/FichaTerceros/Registro/Componentes/Contactos/Contactos.tsx
@@ -1,13 +1,41 @@
+import { Add } from '@mui/icons-material'
+import { Fab, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
+import FormularioNuevoContacto from '../Contactos/FormularioNuevoContacto'
 import CardContact from './CardContact'
 
-const Contactos = (prop: any) => {
+const Contactos = (props: any) => {
+
+  const [verModalNuevoContacto, setverModalNuevoContacto] = useState(false);
+
+  const VerModalNuevoContacto = () => {
+    setverModalNuevoContacto(!verModalNuevoContacto);
+  }
+
   return (
     <>
-      <Stack>
+      <Stack gap={1} direction="row" flexWrap="wrap">
         <CardContact />
+        <CardContact />
+        <CardContact />
+        <Fab color="secondary" variant="extended" onClick={VerModalNuevoContacto}
+          sx={{
+            position: "fixed",
+            bottom: (theme) => theme.spacing(3),
+            right: (theme) => theme.spacing(3)
+          }} >
+          <Add></Add>
+          <Typography>
+            Nuevo Contacto
+          </Typography>
+        </Fab>
       </Stack>
+      {
+        verModalNuevoContacto == true &&
+        <FormularioNuevoContacto estado={verModalNuevoContacto} cambiarEstado={setverModalNuevoContacto} />
+      }
+
     </>
   )
 }
