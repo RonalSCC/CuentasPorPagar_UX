279b7199e93db969dbafd6a9defed96b148e2784
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-24 16:51:57 -0500

    SincoTheme avance

--- src/Theme/Variables.ts
+++ src/Theme/Variables.ts
@@ -1,174 +1,65 @@
-import { blue } from "@mui/material/colors";
-import { Shadows, ThemeOptions } from "@mui/material/styles";
-import { ColorPartial, PaletteColorOptions } from "@mui/material/styles/createPalette";
-import { TypographyOptions } from "@mui/material/styles/createTypography";
-declare module '@mui/material/styles' {
-  interface Palette {
-    blues?: PaletteColorOptions;
-  }
-  interface PaletteOptions {
-    blues?: PaletteColorOptions;
-  }
-}
+import { ThemeOptions } from "@mui/material/styles";
 
-const grey: ColorPartial = {
-  50: "#FAFBFF",
-  100: "#F4F6FA",
-  200: "#EDEFF5",
-  300: "#E6E8F0",
-  400: "#D8DAE5",
-  500: "#C1C4D6",
-  600: "#8F95B2",
-  700: "#696F8C",
-  800: "#474D66",
-  900: "#101840",
-  A100: "#D8DAE5",
-  A200: "#C1C4D6",
-  A400: "#696F8C",
-  A700: "#101840",
-};
-const shadows: Shadows = [
-  "none",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-  "0px 2px 1px -1px rgba(16, 24, 64, 0.1),0px 1px 1px 0px rgba(16, 24, 64, .14),0px 1px 3px 0px rgba(16, 24, 64, .12)",
-];
-const typography: TypographyOptions = {
-  h1: {
-    fontFamily: "Nunito",
-    fontSize: "96px",
-    fontWeight: "300",
-    lineHeight:"112.03px",
-    letterSpacing:"-1.5px"
-  },
-  h2: {
-    fontFamily: "Nunito",
-    fontWeight: "400",
-    fontSize: "60px",
-    lineHeight:"72px",
-    letterSpacing:"-0.5px"
-  },
-  h3: {
-    fontFamily: "Nunito",
-    fontSize: "48px",
-    fontWeight: "500",
-    lineHeight:"56.02px",
-    letterSpacing:"0px"
-  },
-  h4: {
-    fontFamily: "Nunito",
-    fontWeight: "600",
-    fontSize: "34px",
-    lineHeight:"41.99px",
-    letterSpacing:"0.25px"
-  },
-  h5: {
-    fontFamily: "Nunito",
-    fontWeight: "600",
-    fontSize: "24px",
-    lineHeight:"32.02px",
-    letterSpacing:"0"
-  },
-  h6: {
-    fontFamily: "Nunito",
-    fontWeight: "600",
-    fontSize: "20px",
-    lineHeight:"32px",
-    letterSpacing:"0.15px"
-  },
-  subtitle1: {
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    fontSize: "16px",
-    lineHeight:"28px",
-    letterSpacing:"0.15px"
-  },
-  subtitle2: {
-    fontFamily: "Roboto",
-    fontWeight: "500",
-    fontSize: "14px",
-    lineHeight:"21.98px",
-    letterSpacing:"0.1px"
-  },
-  body1: {
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    fontSize: "16px",
-    lineHeight:"24px",
-    letterSpacing:"0.15px"
-  },
-  body2: {
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    fontSize: "14px",
-    lineHeight:"20.02px",
-    letterSpacing:"0.17px"
-  },
-  button: {
-    fontFamily: "Roboto",
-    fontWeight: "500",
-    fontSize: "14px",
-    lineHeight:"24px",
-    letterSpacing:"0.4px"
-  },
-  caption: {
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    fontSize: "12px",
-    lineHeight:"19.92px",
-    letterSpacing:"0.4px"
-  },
-  overline:{
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    fontSize: "12px",
-    lineHeight:"31.92px",
-    letterSpacing:"1px"
-  },
-};
-
-export const light: ThemeOptions = {
-  typography,
-  shadows,
+export const themeOptions: ThemeOptions = {
   palette: {
-    grey,
-    text: {
-      primary: "rgba(16, 24, 64, 0.87)",
-      secondary: "rgba(16, 24, 64, 0.6)",
-      disabled: "rgba(16, 24, 64, 0.38)",
-    },
     primary: {
-      main: "#1E62A1",
+      main: "#1e62a1",
+      light: "#5a8fd3",
       dark: "#003972",
-      light: "#5A8FD3"
+      contrastText: "#ffffff",
     },
     secondary: {
       main: "#0CBBE2",
-      dark: "#008BB0",
-      light: "#67EEFF",
-      contrastText:"#FFFFFF"
+      light: "#67eeff",
+      dark: "#008bb0",
+      contrastText: "#ffffff",
     },
+    text: {
+      primary: "rgba(16,24,64,0.87)",
+      secondary: "rgba(16,24,64,0.6)",
+      disabled: "rgba(16,24,64,0.38)",
+    },
+    error: {
+      main: "#D14343",
+      light: "#d85f5f",
+      dark: "#b51e1e",
+      contrastText: "#ffffff",
+    },
+    warning: {
+      main: "#fb8500",
+      light: "#fc9726",
+      dark: "#f85500",
+      contrastText: "#ffffff",
+    },
+    info: {
+      main: "#2d9fc5",
+      light: "#4dadce",
+      dark: "#1172a3",
+      contrastText: "#ffffff",
+    },
+    success: {
+      main: "#8fc93a",
+      dark: "#60a918",
+      light: "#a0d158",
+      contrastText: "#ffffff",
+    },
+    grey: {
+      50: "#FAFBFF",
+      100: "#F4F6FA",
+      200: "#EDEFF5",
+      300: "#E6E8F0",
+      400: "#D8DAE5",
+      500: "#C1C4D6",
+      600: "#8F95B2",
+      700: "#696F8C",
+      800: "#474D66",
+      900: "#101840",
+      A100: "#D8DAE5",
+      A200: "#C1C4D6",
+      A400: "#696F8C",
+      A700: "#101840",
+    },
+
     action: {
       active: "rgba(16, 24, 64, 0.54)",
       hover: "rgba(16, 24, 64, 0.04)",
@@ -177,119 +68,240 @@ export const light: ThemeOptions = {
       disabledBackground: "rgba(16, 24, 64, 0.12)",
       focus: "rgba(16, 24, 64, 0.12)",
     },
-    error: {
-      main: "#D14343",
-      dark: "#B51E1E",
-      light: "#D85F5F",
-    },
-    warning: {
-      main: "#FB8500",
-      dark: "#F85500",
-      light: "#FC9726",
-    },
-    info: {
-      main: "#2D9FC5",
-      dark: "#1172A3",
-      light: "#4DADCE",
-      contrastText: "#FFF",
-    },
-    success: {
-      main: "#8FC93A",
-      dark: "#60A918",
-      light: "#A0D158",
-      contrastText: "#FFF",
-    },
     background: {
-      paper: "#FBFBFB",
-      default: "#F1F0EE",
+      default: "#f5f5f5",
+      paper: "#fff",
     },
-    blues:{
-      main:blue[100],
-    },
-    divider: "rgba(16, 24, 64, 0.12)",
+    divider: "rgba(16,24,64,0.12)",
   },
+  typography: {
+    fontSize: 11,
+    h6: {
+      fontFamily: "Nunito",
+      fontSize: "1.1rem",
+      fontWeight: 500,
+    },
+    h5: {
+      fontFamily: "Nunito",
+      fontSize: "1.3rem",
+    },
+    h4: {
+      fontFamily: "Nunito",
+      fontSize: "1.8rem",
+    },
+    h3: {
+      fontFamily: "Nunito",
+      fontSize: "2.4rem",
+    },
+    h2: {
+      fontFamily: "Nunito",
+      fontSize: "3.2rem",
+    },
+    h1: {
+      fontFamily: "Nunito",
+      fontSize: "5rem",
+    },
+    fontWeightLight: 300,
+    subtitle2: {
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontWeight: 600,
+    },
+  },
+  spacing: 4,
   components: {
-    MuiChip: {
+    MuiTypography: {
       styleOverrides: {
         root: {
-          borderRadius: 4
-        }
-      }
+          fontSize: 12,
+        },
+      },
     },
-    MuiTab: {
+    MuiButton: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiButtonGroup: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiFab: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiFormControl: {
+      defaultProps: {
+        margin: "dense",
+        size: "small",
+      },
+    },
+    MuiFormHelperText: {
+      defaultProps: {
+        margin: "dense",
+      },
+    },
+    MuiIconButton: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiOutlinedInput: {
       styleOverrides: {
-        root: {
-          textTransform: 'none'
-        }
-      }
+        notchedOutline: {
+          borderColor: "rgba(16, 24, 64, 0.23)",
+        },
+      },
     },
-    MuiButton:{
-      styleOverrides:{
-        root:{
-          textTransform:"none"
-        }
-      }
+    MuiInputBase: {
+      defaultProps: {
+        margin: "dense",
+      },
     },
-    MuiFab:{
-      styleOverrides:{
-        root:{
-          textTransform:"none"
-        }
-      }
-    }
-  }
-};
-export const dark: ThemeOptions = {
-  typography,
-  palette: {
-    grey,
-    text: {
-      secondary: "rgba(255, 255, 255, 0.7)",
-      disabled: "rgba(255, 255, 255, 0.5)",
+    MuiInputLabel: {
+      defaultProps: {
+        margin: "dense",
+      },
     },
-    primary: {
-      main: "#4DADCE",
-      dark: "#2D9FC5",
-      light: "#C0E2EE",
-      contrastText: "rgba(0, 0, 0, 0.87)",
+    MuiRadio: {
+      defaultProps: {
+        size: "small",
+      },
     },
-    secondary: {
-      main: "#BBC2C9",
-      dark: "#8D9AA6",
-      light: "#E4E7EA",
-      contrastText: "rgba(0, 0, 0, 0.87)",
+    MuiSwitch: {
+      defaultProps: {
+        size: "small",
+      },
     },
-    action: {
-      active: "rgba(255, 255, 255, 0.56)",
-      hover: "rgba(255, 255, 255, 0.08)",
-      selected: "rgba(255, 255, 255, 0.16)",
-      disabled: "rgba(255, 255, 255, 0.3)",
-      disabledBackground: "rgba(255, 255, 255, 0.12)",
-      focus: "rgba(255, 255, 255, 0.12)",
+    MuiTextField: {
+      defaultProps: {
+        margin: "dense",
+        size: "small",
+      },
     },
-    error: {
-      main: "#D14343",
-      dark: "#B51E1E",
-      light: "#E57373",
-      contrastText: "#FFF",
+    MuiList: {
+      defaultProps: {
+        dense: true,
+      },
     },
-    warning: {
-      main: "#FB8500",
-      dark: "#F85500",
-      light: "#FC9726",
-      contrastText: "rgba(0, 0, 0, 0.87)",
+    MuiMenuItem: {
+      defaultProps: {
+        dense: true,
+      },
     },
-    info: {
-      main: "#4DADCE",
-      dark: "#2D9FC5",
-      light: "#C0E2EE",
-      contrastText: "rgba(0, 0, 0, 0.87)",
+    MuiTable: {
+      defaultProps: {
+        size: "small",
+      },
     },
-    success: {
-      main: "#8FC93A",
-      dark: "#60A918",
-      light: "#A0D158",
-      contrastText: "rgba(0, 0, 0, 0.87)",
+    MuiPaper: {
+      styleOverrides: {
+        elevation0: "none",
+        elevation1: {
+          boxShadow:
+            "0px 1px 3px rgba(24, 39, 75, 0.12), 0px 1px 1px -1px rgba(24, 39, 75, 0.14), 0px 2px 1px -2px rgba(24, 39, 75, 0.2)",
+        },
+        elevation2: {
+          boxShadow:
+            "0px 1px 5px rgba(24, 39, 75, 0.12), 0px 2px 2px rgba(24, 39, 75, 0.14), 0px 3px 1px -2px rgba(24, 39, 75, 0.2)",
+        },
+        elevation3: {
+          boxShadow:
+            "0px 1px 8px rgba(24, 39, 75, 0.12), 0px 3px 4px rgba(24, 39, 75, 0.14), 0px 3px 3px -2px rgba(24, 39, 75, 0.2)",
+        },
+        elevation4: {
+          boxShadow:
+            "0px 2px 4px -1px rgba(24, 39, 75, 0.2), 0px 4px 5px rgba(24, 39, 75, 0.14), 0px 1px 10px rgba(24, 39, 75, 0.12)",
+        },
+        elevation5: {
+          boxShadow:
+            "0px 3px 5px -1px rgba(24, 39, 75, 0.2), 0px 5px 8px rgba(24, 39, 75, 0.14), 0px 1px 14px rgba(24, 39, 75, 0.12)",
+        },
+        elevation6: {
+          boxShadow:
+            "0px 3px 5px -1px rgba(24, 39, 75, 0.2), 0px 6px 10px rgba(24, 39, 75, 0.14), 0px 1px 18px rgba(24, 39, 75, 0.12)",
+        },
+        elevation7: {
+          boxShadow:
+            "0px 4px 5px -2px rgba(24, 39, 75, 0.2), 0px 7px 10px 1px rgba(24, 39, 75, 0.14), 0px 2px 16px 1px rgba(24, 39, 75, 0.12)",
+        },
+        elevation8: {
+          boxShadow:
+            "0px 5px 5px -3px rgba(24, 39, 75, 0.2), 0px 8px 10px 1px rgba(24, 39, 75, 0.14), 0px 3px 14px 2px rgba(24, 39, 75, 0.12)",
+        },
+        elevation9: {
+          boxShadow:
+            "0px 5px 6px -3px rgba(24, 39, 75, 0.2), 0px 9px 12px 1px rgba(24, 39, 75, 0.14), 0px 3px 16px 2px rgba(24, 39, 75, 0.12)",
+        },
+        elevation10: {
+          boxShadow:
+            "0px 6px 6px -3px rgba(24, 39, 75, 0.2), 0px 10px 14px 1px rgba(24, 39, 75, 0.14), 0px 4px 18px 3px rgba(24, 39, 75, 0.12)",
+        },
+        elevation11: {
+          boxShadow:
+            "0px 6px 7px -4px rgba(24, 39, 75, 0.2), 0px 11px 15px 1px rgba(24, 39, 75, 0.14), 0px 4px 20px 3px rgba(24, 39, 75, 0.12)",
+        },
+        elevation12: {
+          boxShadow:
+            "0px 7px 8px -4px rgba(24, 39, 75, 0.2), 0px 12px 17px 2px rgba(24, 39, 75, 0.14), 0px 5px 22px 4px rgba(24, 39, 75, 0.12)",
+        },
+        elevation13: {
+          boxShadow:
+            "0px 7px 8px -4px rgba(24, 39, 75, 0.2), 0px 13px 19px 2px rgba(24, 39, 75, 0.14), 0px 5px 24px 4px rgba(24, 39, 75, 0.12)",
+        },
+        elevation14: {
+          boxShadow:
+            "0px 7px 9px -4px rgba(24, 39, 75, 0.2), 0px 14px 21px 2px rgba(24, 39, 75, 0.14), 0px 5px 26px 4px rgba(24, 39, 75, 0.12)",
+        },
+        elevation15: {
+          boxShadow:
+            "0px 8px 9px -5px rgba(24, 39, 75, 0.2), 0px 15px 22px 2px rgba(24, 39, 75, 0.14), 0px 6px 28px 5px rgba(24, 39, 75, 0.12)",
+        },
+        elevation16: {
+          boxShadow:
+            "0px 8px 10px -5px rgba(24, 39, 75, 0.2), 0px 16px 24px 2px rgba(24, 39, 75, 0.14), 0px 6px 30px 5px rgba(24, 39, 75, 0.12)",
+        },
+        elevation17: {
+          boxShadow:
+            "0px 8px 11px -5px rgba(24, 39, 75, 0.2), 0px 17px 26px 2px rgba(24, 39, 75, 0.14), 0px 6px 32px 5px rgba(24, 39, 75, 0.12)",
+        },
+        elevation18: {
+          boxShadow:
+            "0px 9px 11px -5px rgba(24, 39, 75, 0.2), 0px 18px 28px 2px rgba(24, 39, 75, 0.14), 0px 7px 34px 6px rgba(24, 39, 75, 0.12)",
+        },
+        elevation19: {
+          boxShadow:
+            "0px 9px 12px -6px rgba(24, 39, 75, 0.2), 0px 19px 29px 2px rgba(24, 39, 75, 0.14), 0px 7px 36px 6px rgba(24, 39, 75, 0.12)",
+        },
+        elevation20: {
+          boxShadow:
+            "0px 10px 13px -6px rgba(24, 39, 75, 0.2), 0px 20px 31px 3px rgba(24, 39, 75, 0.14), 0px 8px 38px 7px rgba(24, 39, 75, 0.12)",
+        },
+        elevation21: {
+          boxShadow:
+            "0px 10px 13px -6px rgba(24, 39, 75, 0.2), 0px 21px 33px 3px rgba(24, 39, 75, 0.14), 0px 8px 40px 7px rgba(24, 39, 75, 0.12)",
+        },
+        elevation22: {
+          boxShadow:
+            "0px 10px 14px -6px rgba(24, 39, 75, 0.2), 0px 22px 35px 3px rgba(24, 39, 75, 0.14), 0px 8px 42px 7px rgba(24, 39, 75, 0.12)",
+        },
+        elevation23: {
+          boxShadow:
+            "0px 11px 14px -7px rgba(24, 39, 75, 0.2), 0px 23px 36px 3px rgba(24, 39, 75, 0.14), 0px 9px 44px 8px rgba(24, 39, 75, 0.12)",
+        },
+        elevation24: {
+          boxShadow:
+            "0px 11px 15px -7px rgba(24, 39, 75, 0.2), 0px 24px 38px 3px rgba(24, 39, 75, 0.14), 0px 9px 46px 8px rgba(24, 39, 75, 0.12)",
+        },
+      },
     },
   },
 };
