6f67545003f524543e948ddae41cdb6dd6f4423f
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-24 09:12:05 -0500

    nueva rama custom-css

--- src/Aplicaciones/FichaTerceros/Route.tsx
+++ src/Aplicaciones/FichaTerceros/Route.tsx
@@ -1,5 +1,5 @@
 import type { RouteObject } from "react-router-dom";
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 import MarcoTerceros from "./Registro/Componentes/MarcoTerceros";
 import FormularioRegistroTercero from "./Registro/Componentes/NuevoRegistro/FormularioRegistroTercero";
 import SinSeleccion from "./Registro/Componentes/SinSeleccion";
@@ -7,43 +7,37 @@ import Terceros from "./Registro/Componentes/Terceros";
 import MarcoTerceroProveedor from "./Registro/Contextos/MarcoTercerosProveedor";
 import RoutesMarcoTerceros from "./Registro/Route";
 
-const CambiarTerceroMientras = ()=>{
-
-}
-const RoutesTerceros:RouteObject[] = [
-    {
-        children:[
-            {
-                path:"MarcoTerceros",
-                element: 
-                    <>
-                        <MarcoTerceroProveedor>
-                            <MarcoTerceros/>
-                        </MarcoTerceroProveedor>
-                    </>
-                ,
-                children: RoutesMarcoTerceros
-            },
-            {
-                path:"*",
-                element: <SinSeleccion/>,
-                index:true
-            },
-            {
-                path:"FormularioRegistro",
-                element: <FormularioRegistroTercero/>
-            }
-        ]
-    }
+const CambiarTerceroMientras = () => {};
+const RoutesTerceros: RouteObject[] = [
+  {
+    children: [
+      {
+        path: "MarcoTerceros",
+        element: (
+          <>
+            <MarcoTerceroProveedor>
+              <MarcoTerceros />
+            </MarcoTerceroProveedor>
+          </>
+        ),
+        children: RoutesMarcoTerceros,
+      },
+      {
+        path: "*",
+        element: <SinSeleccion />,
+        index: true,
+      },
+      {
+        path: "FormularioRegistro",
+        element: <FormularioRegistroTercero />,
+      },
+    ],
+  },
 ];
 
-export const RoutesTercerosElement = ()=>{
-    const RoutesTecerosElement = useRoutes(RoutesTerceros);
-    return <>
-       {
-            RoutesTecerosElement
-       } 
-    </>
-}
+export const RoutesTercerosElement = () => {
+  const RoutesTecerosElement = useRoutes(RoutesTerceros);
+  return <>{RoutesTecerosElement}</>;
+};
 
 export default RoutesTerceros;
