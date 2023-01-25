e08f36cb369817465ded66c3ccfc21711deb9619
Ronal Santiago Castaño Chaparro <ronal.castano@sinco.com.co>
2023-01-25 08:36:50 -0500

    Change FormControl margin

---
+++ src/Theme/componentes.tsx
@@ -0,0 +1,100 @@
+import { Components } from "@mui/material";
+
+// CONFIGURACION COMPONENTES
+
+// INPUTS
+export const Componentes: Components = {
+  // // TEXTFIELD INPUT BASE
+  // MuiTextField: {
+  //   defaultProps: {
+  //     InputProps: {
+        
+  //     },
+  //     size: "small",
+  //   },
+  // },
+  // // INPUT LABEL
+  // MuiInputLabel: {
+  //   styleOverrides: {
+  //     filled: {
+  //       "&.Mui-focused": {
+  //         top: -4,
+  //       },
+  //     },
+  //     standard: {
+  //       "&.Mui-focused": {
+  //         top: 3,
+  //       },
+  //     },
+  //     outlined: {
+  //       "&.MuiInputLabel-outlined": {
+  //         top: -6,
+  //       },
+  //       "&.Mui-focused": {
+  //         top: 3,
+  //       },
+  //     },
+  //     root: {
+  //       "&.MuiInputLabel-root": {
+  //         top: 0,
+  //       },
+  //     },
+  //     sizeSmall: {
+  //       fontSize: 13,
+  //       "&.MuiInputLabel-root": {
+  //         top: -1,
+  //         "&.Mui-focused": {
+  //           top: 0,
+  //         },
+  //         "&.MuiInputLabel-outlined": {
+  //           top: 0,
+  //           "&.Mui-focused": {
+  //             top: 3,
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
+  //  // INPUT BASE
+  // MuiInputBase: {
+  //   styleOverrides: {
+  //     root: {
+  //       "& .MuiFilledInput-input": {
+  //         height: 14,
+  //       },
+  //       "& .MuiOutlinedInput-input": {
+  //         height: 5,
+  //       },
+  //     },
+  //     sizeSmall: {
+  //       fontSize: 13,
+  //       ".MuiInputBase-inputSizeSmall	": {
+  //         height: ".9rem",
+  //       },
+  //       "& .MuiFilledInput-input": {
+  //         height: 13,
+  //       },
+  //       "& .MuiOutlinedInput-input": {
+  //         height: 15,
+  //       },
+  //     },
+  //   },
+  // },
+
+  // // FORM CONTROL
+  // MuiFormControl: {
+  //   styleOverrides: {
+  //     root: {
+  //       "&. MuiFormControlLabel-label": {
+  //         fontSize: "13px",
+  //       },
+  //     },
+  //   },
+  //   defaultProps: {
+  //     size: "small",
+  //   },
+  // },
+};
+
+export const components = Componentes;
