8a1fea6d47bb4baa8859f74d7355850a72b654f7
CPZ_CristianPerez <cristian.perez@sinco.com.co>
2023-03-30 15:54:56 -0500

    Avance de consumo

--- src/Aplicaciones/FichaTerceros/Registro/Componentes/ConfiguracionAvanzada/EmbargoEndoso/EmbargoEndoso.tsx
+++ src/Aplicaciones/FichaTerceros/Registro/Componentes/ConfiguracionAvanzada/EmbargoEndoso/EmbargoEndoso.tsx
@@ -1,38 +1,182 @@
 import { Info } from '@mui/icons-material'
-import { Stack, Card, Alert, TextField, Divider, FormControlLabel, FormGroup, Checkbox, Icon } from '@mui/material'
+import { Stack, Card, Alert, TextField, Divider, FormControlLabel, FormGroup, Checkbox, Icon, AlertTitle, Autocomplete, MenuItem } from '@mui/material'
 import Image from 'mui-image'
-import React from 'react'
+import { useContext, useEffect, useState } from 'react';
+import { SendRequest } from '../../../../../../Consumos/Request';
+import { TercerosContexto } from '../../../../Contextos/TercerosContexto';
+import { PropsTerceroContexto } from '../../../../Contextos/TercerosProveedor';
+import { Controller, useForm } from 'react-hook-form'
+import { ITercero } from '../../NuevoRegistro/_FormularioTercero';
+import { DevTool } from '@hookform/devtools';
+
+export interface ITerceroEndoso {
+    tdaTerId: number,
+    tdaEmbargado: boolean,
+    tdaBeneficiario: string,
+    tdaTerBenTransferencia: string
+}
 
 const EmbargoEndoso = () => {
+
+    const { propsTercerosContexto }: { propsTercerosContexto: PropsTerceroContexto } = useContext<any>(TercerosContexto);
+    const [TercerosList, setTercerosList] = useState<Array<any>>([])
+
+    const { control, setValue } = useForm({
+        defaultValues:{
+            tdaBeneficiario:"",
+            tdaEmbargado:false,
+            tdaTerBenTransferencia:"",
+
+        },
+        mode: "onChange"
+    })
+
+    const ConsultarTerceros = () => {
+        SendRequest.get({
+            API: "CONFIGURACION",
+            URLServicio: "/ConsultasGenerales/ConsultarInformacionListas",
+            Body: {
+                Clave: "TercerosClienteProveedor"
+            }
+        }).then(respuesta => {
+            if (respuesta != null && respuesta.ok) {
+                setTercerosList(respuesta.datos)
+            }
+        })
+    }
+
+    const EditarTerceroEndoso = () => {
+
+    }
+
+    const ConsultarTerceroEndoso = () => {
+        SendRequest.get({
+            API: "CUENTASPORPAGAR",
+            URLServicio: "/EmbargoEndosoTerceros/Consultar_EmbargoEndosoTercero",
+            Body: {
+                TerId: propsTercerosContexto.TerceroSeleccionadoLista?.TerID
+            }
+        }).then(respuesta => {
+            if (respuesta != null && respuesta.ok == true) {
+                setValue("tdaEmbargado", respuesta.datos.tdaEmbargado || false)
+                setValue("tdaBeneficiario", respuesta.datos.tdaBeneficiario || "")
+                setValue("tdaTerBenTransferencia", respuesta.datos.tdaTerBenTransferencia || "")
+            }
+        }).catch(respuesta => {
+
+            propsTercerosContexto.CambiarAlertas(
+                respuesta.errores.map((x: any) => {
+                    return <>
+                        <Alert
+                            key={x.descripcion}
+                            severity="warning"
+                            onClose={() => propsTercerosContexto.CerrarAlertas()}
+                        >
+                            <AlertTitle>Error</AlertTitle>
+                            {x.descripcion}
+                        </Alert>
+                    </>;
+                })
+            );
+        })
+    }
+
+    useEffect(() => {
+        ConsultarTerceros()
+    }, [])
+
+    useEffect(() => {
+        ConsultarTerceroEndoso()
+    }, [])
+
     return (
         <>
             <Stack width={"100%"} border={0.8} borderColor="#F1F0EE" borderRadius={0.5}>
                 <Card>
-                    <Stack p={2} gap={0.8}>
+                    <Stack p={2} gap={1}>
                         <Stack>
-                            <Alert icon={<Image width={47.78} fit='cover' src="Imagenes/Terceros/AccountingDocuments.svg" />} severity="info"
-                            >
+                            <Alert
+                                icon={<Image
+                                    width={47.78}
+                                    fit='cover'
+                                    src="Imagenes/Terceros/AccountingDocuments.svg" />}
+                                severity="info"
+                                sx={{ alignItems: "center" }}>
                                 Aplica para el endoso de documentos contables
                             </Alert>
                         </Stack>
                         <Stack>
-                            <TextField size="small" label="Beneficiario endoso" fullWidth></TextField>
+                            <Controller
+                                control={control}
+                                name="tdaBeneficiario"
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        size="small"
+                                        label="Beneficiario endoso"
+                                        fullWidth></TextField>
+                                )}
+                            />
+
                         </Stack>
                         <Divider orientation='horizontal' />
                         <Stack px={1} direction="row" alignItems="center">
                             <FormGroup>
-                                <FormControlLabel control={<Checkbox size="small" defaultChecked={false} />} label="Tercero Embargado" />
+                                <Controller
+                                    control={control}
+                                    name="tdaEmbargado"
+                                     
+                                    render={({ field }) => (
+                                        <FormControlLabel
+                                            control={<Checkbox {...field} name="tdaEmbargado" size="small" checked={field.value} />}
+                                            label="Tercero Embargado" />
+                                    )}
+                                />
                             </FormGroup>
                             <Icon>
                                 <Info fontSize="small" color="disabled"></Info>
                             </Icon>
                         </Stack>
                         <Stack>
-                            <TextField size="small" label="Beneficiario del embargo" fullWidth></TextField>
+                            <Controller
+                                control={control}
+                                name="tdaTerBenTransferencia"
+                                render={({ field: { value, onChange, ref } }) => (
+                                    <Autocomplete
+                                        value={
+                                            value ? (TercerosList.find((option) => value === option.TerID) ?? null) : null
+                                        }
+                                        getOptionLabel={(option) => option.TerNit + " - " + option.TerNombre}
+                                        onChange={(e: any, newValue) => {
+                                            onChange(newValue ? newValue.TerID : null);
+                                        }}
+                                        options={TercerosList}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label={"Beneficiiaro"}
+                                                inputRef={ref}
+                                            />
+                                        )}
+                                    />
+                                    // <TextField
+                                    //     {...field}
+                                    //     name="tdaBeneficiario"
+                                    //     size="small"
+                                    //     label="Beneficiario del embargo"
+                                    //     fullWidth
+                                    //     select
+                                    //     >{
+                                    //         TercerosList.map((ter) => <MenuItem key={ter.TerID} value={ter.TerID}>{ter.TerNit} - {ter.TerNombre}</MenuItem>)
+                                    //     }
+                                    // </TextField>
+                                )}
+                            />
                         </Stack>
                     </Stack>
                 </Card>
-            </Stack>
+            </Stack >
+            <DevTool control={control}></DevTool>
         </>
     )
 }
