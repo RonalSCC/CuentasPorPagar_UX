f0aa0cc242b80289e4912990cb2791b1f5e84c15
CPZ_CristianPerez <cristian.perez@sinco.com.co>
2023-03-31 10:16:45 -0500

    Finalización Conumo, implementó autocomplete prov

--- src/Aplicaciones/FichaTerceros/Registro/Componentes/ConfiguracionAvanzada/EmbargoEndoso/EmbargoEndoso.tsx
+++ src/Aplicaciones/FichaTerceros/Registro/Componentes/ConfiguracionAvanzada/EmbargoEndoso/EmbargoEndoso.tsx
@@ -1,5 +1,5 @@
 import { Info } from '@mui/icons-material'
-import { Stack, Card, Alert, TextField, Divider, FormControlLabel, FormGroup, Checkbox, Icon, AlertTitle, Autocomplete, MenuItem } from '@mui/material'
+import { Stack, Card, Alert, TextField, Divider, FormControlLabel, FormGroup, Checkbox, Icon, AlertTitle, Autocomplete, MenuItem, Button, Tooltip } from '@mui/material'
 import Image from 'mui-image'
 import { useContext, useEffect, useState } from 'react';
 import { SendRequest } from '../../../../../../Consumos/Request';
@@ -21,35 +21,63 @@ const EmbargoEndoso = () => {
     const { propsTercerosContexto }: { propsTercerosContexto: PropsTerceroContexto } = useContext<any>(TercerosContexto);
     const [TercerosList, setTercerosList] = useState<Array<any>>([])
 
-    const { control, setValue } = useForm({
-        defaultValues:{
-            tdaBeneficiario:"",
-            tdaEmbargado:false,
-            tdaTerBenTransferencia:"",
+    const { control, setValue, handleSubmit, watch } = useForm({
+        defaultValues: {
+            tdaBeneficiario: "",
+            tdaEmbargado: false,
+            tdaTerBenTransferencia: "",
 
         },
         mode: "onChange"
     })
 
-    const ConsultarTerceros = () => {
+    const tdaEmbargado = watch("tdaEmbargado")
+
+    const ConsultarTerceros = (data: ITerceroEndoso) => {
         SendRequest.get({
             API: "CONFIGURACION",
             URLServicio: "/ConsultasGenerales/ConsultarInformacionListas",
             Body: {
-                Clave: "TercerosClienteProveedor"
+                Clave: "TercerosClienteProveedor",
+                Condiciones: JSON.stringify({
+                    TextoFiltrado: data.tdaTerBenTransferencia
+                })
             }
         }).then(respuesta => {
             if (respuesta != null && respuesta.ok) {
                 setTercerosList(respuesta.datos)
+                setValue("tdaTerBenTransferencia", `${data.tdaTerBenTransferencia}` || "")
             }
         })
     }
 
-    const EditarTerceroEndoso = () => {
+    const EditarTerceroEndoso = (data: any) => {
+        SendRequest.put({
+            API: "CUENTASPORPAGAR",
+            URLServicio: "/EmbargoEndosoTerceros/Actualizar_EmbargoEndosoTercero",
+            Body: {
+                TDATerId: propsTercerosContexto.TerceroSeleccionadoLista?.TerID,
+                TDABeneficiario: data.tdaBeneficiario,
+                TDAEmbargado: data.tdaEmbargado,
+                TDATerBenTransferencia: data.tdaTerBenTransferencia
+            }
+        }).then(respuesta => {
+            if (respuesta != null && respuesta.ok) {
+                [1].map(() => (
+                    <Alert
+                        key={1}
+                        severity="success"
+                        onClose={() => propsTercerosContexto.CerrarAlertas()}
+                    >
+                        Información actualizada con éxito
 
+                    </Alert>
+                ))
+            }
+        })
     }
 
-    const ConsultarTerceroEndoso = () => {
+    const ConsultarTerceroEndoso = (callback: Function) => {
         SendRequest.get({
             API: "CUENTASPORPAGAR",
             URLServicio: "/EmbargoEndosoTerceros/Consultar_EmbargoEndosoTercero",
@@ -58,9 +86,11 @@ const EmbargoEndoso = () => {
             }
         }).then(respuesta => {
             if (respuesta != null && respuesta.ok == true) {
-                setValue("tdaEmbargado", respuesta.datos.tdaEmbargado || false)
+
                 setValue("tdaBeneficiario", respuesta.datos.tdaBeneficiario || "")
-                setValue("tdaTerBenTransferencia", respuesta.datos.tdaTerBenTransferencia || "")
+                setValue("tdaEmbargado", respuesta.datos.tdaEmbargado || false)
+                callback(respuesta.datos);
+
             }
         }).catch(respuesta => {
 
@@ -82,16 +112,14 @@ const EmbargoEndoso = () => {
     }
 
     useEffect(() => {
-        ConsultarTerceros()
-    }, [])
-
-    useEffect(() => {
-        ConsultarTerceroEndoso()
+        ConsultarTerceroEndoso((data: ITerceroEndoso) => {
+            ConsultarTerceros(data)
+        })
     }, [])
 
     return (
         <>
-            <Stack width={"100%"} border={0.8} borderColor="#F1F0EE" borderRadius={0.5}>
+            <Stack width={"100%"} height={"100%"} justifyContent="space-between" border={0.8} borderColor="#F1F0EE" borderRadius={0.5}>
                 <Card>
                     <Stack p={2} gap={1}>
                         <Stack>
@@ -125,7 +153,7 @@ const EmbargoEndoso = () => {
                                 <Controller
                                     control={control}
                                     name="tdaEmbargado"
-                                     
+
                                     render={({ field }) => (
                                         <FormControlLabel
                                             control={<Checkbox {...field} name="tdaEmbargado" size="small" checked={field.value} />}
@@ -133,49 +161,81 @@ const EmbargoEndoso = () => {
                                     )}
                                 />
                             </FormGroup>
+                            <Tooltip title="Solo aplica para cheque" placement='top' arrow >
                             <Icon>
                                 <Info fontSize="small" color="disabled"></Info>
                             </Icon>
+                            </Tooltip>
                         </Stack>
-                        <Stack>
-                            <Controller
-                                control={control}
-                                name="tdaTerBenTransferencia"
-                                render={({ field: { value, onChange, ref } }) => (
-                                    <Autocomplete
-                                        value={
-                                            value ? (TercerosList.find((option) => value === option.TerID) ?? null) : null
-                                        }
-                                        getOptionLabel={(option) => option.TerNit + " - " + option.TerNombre}
-                                        onChange={(e: any, newValue) => {
-                                            onChange(newValue ? newValue.TerID : null);
-                                        }}
-                                        options={TercerosList}
-                                        renderInput={(params) => (
-                                            <TextField
-                                                {...params}
-                                                label={"Beneficiiaro"}
-                                                inputRef={ref}
-                                            />
-                                        )}
-                                    />
-                                    // <TextField
-                                    //     {...field}
-                                    //     name="tdaBeneficiario"
-                                    //     size="small"
-                                    //     label="Beneficiario del embargo"
-                                    //     fullWidth
-                                    //     select
-                                    //     >{
-                                    //         TercerosList.map((ter) => <MenuItem key={ter.TerID} value={ter.TerID}>{ter.TerNit} - {ter.TerNombre}</MenuItem>)
-                                    //     }
-                                    // </TextField>
-                                )}
-                            />
-                        </Stack>
+
+                        {
+                            tdaEmbargado &&
+                            <Stack>
+                                <Controller
+                                    control={control}
+                                    name="tdaTerBenTransferencia"
+                                    render={({ field: { value, onChange, ref } }) => (
+                                        <Autocomplete
+                                            value={
+                                                value ? (TercerosList.find((option) => value === option.TerID) ?? null) : null
+                                            }
+                                            getOptionLabel={(option) => option.TerNit + " - " + option.TerNombre}
+                                            onChange={(e: any, newValue) => {
+                                                onChange(newValue ? newValue.TerID : null);
+                                            }}
+                                            options={TercerosList}
+                                            renderInput={(params) => (
+                                                <TextField
+                                                    {...params}
+                                                    label={"Beneficiiaro"}
+                                                    inputRef={ref}
+                                                />
+                                            )}
+                                        />
+                                    )}
+                                />
+                            </Stack>
+                        }
                     </Stack>
                 </Card>
+                <Stack
+                    direction={"row"}
+                    flexWrap="wrap"
+                    position={"sticky"}
+                    width="100%"
+                    bottom={0}
+                    right={0}
+                    sx={{ backgroundColor: "primary.contrastText" }}
+                >
+                    <Divider sx={{ width: "100%" }} orientation='horizontal' />
+                    <Stack
+                        width="100%"
+                        direction={"row"}
+                        paddingY={1.5}
+                        paddingX={3}
+                        gap={1.5}
+                        justifyContent="flex-end">
+
+                        <Button
+                            variant="text"
+                            size="small"
+                        // onClick={()=> MetodoCancelar()}
+                        >
+                            Cancelar
+                        </Button>
+
+                        <Button
+                            variant="contained"
+                            size="small"
+                            color='primary'
+                            onClick={handleSubmit(EditarTerceroEndoso)}
+                        >
+                            Guardar
+                        </Button>
+                    </Stack>
+                </Stack>
             </Stack >
+
             <DevTool control={control}></DevTool>
         </>
     )
